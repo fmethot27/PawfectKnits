30375e52ce66506064844df01c7a405863e3746f
Francis Méthot <154344037+fmethot27@users.noreply.github.com>
2024-02-05 09:43:53 -0500

    Update script.js
    I forgot to change the value of the redirection in the webpage

--- javascript/script.js
+++ javascript/script.js
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", function () {
       submitButton.addEventListener("click", function () {
         if (validateLogin()) {
           alert("Login successful!");
-        window.location.href = 'homepage.html';
+        window.location.href = 'index.html';
      }
          
     });
